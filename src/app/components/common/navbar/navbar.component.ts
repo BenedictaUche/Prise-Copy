adebb25327cb3bf1728c19d0dd68fb8c1f258d19
Benedicta Onyebuchi <benedictauche.onyebuchi@gmail.com>
2024-03-11 11:28:22 +0100

    links

--- src/app/components/common/navbar/navbar.component.ts
+++ src/app/components/common/navbar/navbar.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
-  title = 'Prise-Website';
+export class NavbarComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  scrollToAboutPrise() {
+    const aboutPriseElement = document.getElementById('about-prise');
+    if (aboutPriseElement) {
+      aboutPriseElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
 }
